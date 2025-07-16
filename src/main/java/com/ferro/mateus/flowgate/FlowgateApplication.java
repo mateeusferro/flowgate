385146ce28e0990681cadce3a514fd748b343543
mateus <mateus.03fernandesaraujo@gmail.com>
2025-07-16 20:01:10 -0300

    init: initial commit

---
+++ src/main/java/com/ferro/mateus/flowgate/FlowgateApplication.java
@@ -0,0 +1,13 @@
+package com.ferro.mateus.flowgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowgateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowgateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ferro/mateus/flowgate/FlowgateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferro.mateus.flowgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowgateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
